15ab0f101f2aafb3e96313a199f975e96a86861d
andriesvdw1 <andriesvdw1@gmail.com>
2025-05-08 12:05:04 +0200

    Make navbar component only visible once the user scrolls past a certain point in the browser.

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-function Navbar({links, hrefs}) {
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+
+function Navbar({ links, hrefs }) {
+  const [displayNavbar, setDisplayNavbar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setDisplayNavbar(true);
+      } else {
+        setDisplayNavbar(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='navbar'>
-        {links.map((link, index) => (
-            <a className='nav-link' key={index} href={hrefs[index]}>{link}</a> 
-        ))}
+    <div className={`navbar ${displayNavbar ? 'visible' : 'hidden'}`}>
+      {links.map((link, index) => (
+        <a className="nav-link" key={index} href={hrefs[index]}>{link}</a>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
